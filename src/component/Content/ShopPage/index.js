35edc7f3e32b271b8437385e80ee8ca6c0f927b2
Trần Phi Hùng <hungtk1402@gmail.com>
2024-10-04 02:24:31 +0700

    Male fashion

--- src/component/Content/ShopPage/index.js
+++ src/component/Content/ShopPage/index.js
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom'
+import './ShopPage.css'
+
 const ShopPage = () => {
     return (
         <>
+            <section className='breadcrumb-option'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-lg-12'>
+                            <div className='breadcrumb__text'>
+                                <h4>Shop</h4>
+                                <div className='breadcrumb__links'>
+                                    <Link to='/'>Home</Link>
+                                    <span>Shop</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             Shop Page
         </>
     )
